4c37499d3610f8f478bf9b365ddcbca4c0361ff4
syz970622 <yunzhous@andrew.cmu.edu>
2021-04-24 12:41:04 -0400

    base version 04/24
    drawcurve doesn't work

--- audio manipulation curve/sketch.js
+++ audio manipulation curve/sketch.js
@@ -1,23 +1,28 @@
-let sound, amplitude, frequency, fft, speed;
-let sliderLength = 500;
-let sliderStart = 100; sliderStop = 500;
-let amplitudeX = 100, amplitudeY = 200;
-let frequencyX = 100, frequencyY = 350;
-let reverbX = 100, reverbY = 500;
-let controlRadius = 10;
-let ampLevel;
+var sound, amplitude, frequency, fft, speed;
 
-//drawMoveCircle
+//slider variables
+var sliderLength = 500;
+var sliderStart = 100; sliderStop = 500;
+    //ball on the slider
+var amplitudeX = 100, amplitudeY = 200;
+var frequencyX = 100, frequencyY = 350;
+var reverbX = 100, reverbY = 500;
+var sliderBallRadius = 10;
+
+//BounceCircle variables
 var ellipseR = 25;
 var ellipseX = 300, ellipseY = 400;
-var rectHOffset = 0; rectWOffset = 0;
+    //ellipse velocity
 var ellipseDeltaX = 0; ellipseDeltaY = 0;
 
-var sliderButtonClicked = false;
-var mouseActionButtonClicked = false;
-var moveCircleButtonClicked = false;
+//drawCurve variables
+var xAxis = [];
+var yAxis = [];
+
+//determine manipulation method
+var sliderButtonClicked = true;
 var bounceCircleButtonClicked = false;
-var drawCurveButtonClicked = true;
+var drawCurveButtonClicked = false;
 
 
 function preload() {
@@ -34,52 +39,38 @@ function setup() {
     sound.disconnect();
     sound.connect(filter);
     reverb.process(sound, 3, 2);
+
+    //load image of diff visualization buttons
     img = loadImage('assets/button.png');
 
 }
 
 function draw() {
-
+    //to make drawCurve work, move "background(0)" to function "setup()";
+    //otherwise background will cover the line we draw
+    //but if we move "background(0)" to function "setup()", the other functions won't work
+    //might have to write "drawCurve in another js doc
     background(0);
 
-    drawSoundButton();
-
+    //click "PlaySound" to play/pause audio
+    document.getElementById('soundButton').onclick = function() {
+        toggleSound();
+    }
+    //switch manipulation method to slider
     document.getElementById('sliderButton').onclick = function() {
         sliderButtonClicked = true;
-        mouseActionButtonClicked = false;
-        moveCircleButtonClicked = false;
         bounceCircleButtonClicked = false;
         drawCurveButtonClicked = false;
     }
-
-    document.getElementById('mouseActionButton').onclick = function() {
-        sliderButtonClicked = false;
-        mouseActionButtonClicked = true;
-        moveCircleButtonClicked = false;
-        bounceCircleButtonClicked = false;
-        drawCurveButtonClicked = false;
-    }
-
-    document.getElementById('moveCircle').onclick = function() {
-        sliderButtonClicked = false;
-        mouseActionButtonClicked = false;
-        moveCircleButtonClicked = true;
-        bounceCircleButtonClicked = false;
-        drawCurveButtonClicked = false;
-    }
-
+    //switch manipulation method to bounce circle
     document.getElementById('bounceCircle').onclick = function() {
         sliderButtonClicked = false;
-        mouseActionButtonClicked = false;
-        moveCircleButtonClicked = false;
         bounceCircleButtonClicked = true;
         drawCurveButtonClicked = false;
     }
-
+    //switch manipulation method to draw curve
     document.getElementById('drawCurve').onclick = function() {
         sliderButtonClicked = false;
-        mouseActionButtonClicked = false;
-        moveCircleButtonClicked = false;
         bounceCircleButtonClicked = false;
         drawCurveButtonClicked = true;
     }
@@ -87,14 +78,6 @@ function draw() {
     if (sliderButtonClicked == true){
         drawSliders();
         sound.rate(1);
-    }
-
-    if (mouseActionButtonClicked == true){
-        drawMouseAction();
-    }
-
-    if (moveCircleButtonClicked == true){
-        drawMoveCircle();
     }
 
     if(bounceCircleButtonClicked == true){
@@ -107,29 +90,111 @@ function draw() {
 
 }
 
+//math equations to draw one visualization
+function drawVisualization1() {
+    //get&map amplitude
+    var ampLevel = amplitude.getLevel();
+    var drawLine = map(ampLevel, 0, 0.1, 100, 800);
+    //astroid
+    beginShape();
+    stroke(163, 214, 245);
+    strokeWeight(0.5);
+    noFill();
+    translate(width / 2, height / 2);
+    for (var i = 0; i < drawLine / 2; i++) { //mouseX controls number of curves
+        LimMouseX = constrain(drawLine*2, 0, width);
+        var a = map(LimMouseX, 0, width, 10, 80); //relate to mouseX
+        var theta = map(i, 0, drawLine*2, 20, 360);
+        var x = 2 * a * cos(theta) + a * cos(2 * theta);
+        var y = 2 * a * sin(theta) - a * sin(2 * theta);
+        vertex(x, y);
+        endShape();
+        rotate(drawLine*2); //rotate according to position of mouseX
+    }
+}
+
+//math equations to draw one visualization
+function drawVisualization2(parameter1, parameter2){
+
+    //Epicycloid Involute
+    beginShape();
+    stroke(242, 194, 203);
+    strokeWeight(0.5);
+    noFill();
+    for (var i = 0; i < parameter1; i ++){ //mouseX controls number of curves
+        LimMouseX = constrain(parameter1, 0, width);
+        var a = map(LimMouseX, 0, width, 0, 80); //relate to mouseX
+        var theta = map(i, 0, parameter1/5, 20, 360);
+        var b = map(parameter2, 0, height, 0, 50);
+        var x2 = (a+b)*cos(theta) - b*cos(((a+b)/b)*theta);
+        var y2 = (a+b)*sin(theta) - b*sin(((a+b)/b)*theta);
+        vertex(x2, y2);
+        endShape();
+    }
+}
+
+//draw a curve to manipulate sound
 function drawCurve(){
     //draw canvas
-    var rectW = 500; rectH = 500;
-    var startX = 50; startY = 150;
-    var centerX = rectW/2 + startX; centerY = rectH/2 + startY;
-    fill(255);
+    var rectW = 500; rectH = 500;     //canvas width & height
+    var startX = 50; startY = 150;    //canvas upper left corner
+
+    noFill();
+    stroke(255);
+    //used quad for potentially move four corners as ball moving
+    //inspired by https://learningsynths.ableton.com/
     quad(startX, startY,
         startX + rectW, startY,
         startX + rectW, startY + rectH,
         startX, startY + rectH);
+    //
+    var volume = 0; freq = 0;
 
-    stroke(0);
+    IsDrawingCurve()
+    //calculate sum of x and y coordinates in the list
+    for (var i = 0; i <xAxis.length ; i++){
+        volume += xAxis[i];
+        freq += yAxis[i];
+    }
+
+    //change volume
+    volume = map(volume, 0, 20000, 0, 1);
+    volume = constrain(volume, 0, 1);
+    sound.amp(volume);
+    //change frequency
+    freq = map(freq, 0, 20000, 20,20000);
+    freq = constrain(freq,20,20000);
+    filter.freq(freq);
+}
+
+//action when user is drawing a curve
+function IsDrawingCurve(){
+    stroke(255);
     if (mouseIsPressed === true) {
         line(mouseX, mouseY, pmouseX, pmouseY);
+        //as mouse is draggin to draw, add x and y coordinates to list
+        append(xAxis, mouseX);
+        append(yAxis, mouseY);
     }
 }
 
+//reset drawCurve variables
+function mousePressed(){
+    xAxis = [];
+    volume = 0;
+    yAxis = [];
+    freq = 0;
+}
+
+//bounce circle to manipulate sound
 function drawBounceCircle(){
     //draw canvas
-    var rectW = 500; rectH = 500;
-    var startX = 50; startY = 150;
-    var centerX = rectW/2 + startX; centerY = rectH/2 + startY;
+    var rectW = 500; rectH = 500;   //canvas width & height
+    var startX = 50; startY = 150;  //canvas upper left corner
+
     fill(255);
+    //used quad for potentially move four corners as ball moving
+    //inspired by https://learningsynths.ableton.com/
     quad(startX, startY,
         startX + rectW, startY,
         startX + rectW, startY + rectH,
@@ -166,120 +231,10 @@ function drawBounceCircle(){
 
 }
 
-function drawMoveCircle(){
-    //draw canvas
-    var rectW = 500; rectH = 500;
-    var startX = 50; startY = 150;
-    var centerX = rectW/2 + startX; centerY = rectH/2 + startY;
-    fill(255);
-    quad(startX - rectWOffset, startY - rectHOffset,
-        startX + rectW + rectWOffset, startY - rectHOffset,
-        startX + rectW + rectWOffset, startY + rectH + rectHOffset,
-        startX - rectWOffset, startY + rectH + rectHOffset);
-
-    //draw circle
-    fill(0);
-    ellipse(ellipseX,ellipseY,ellipseR*2);
-    //drag to move circle
-    if (mouseIsPressed && mouseX > ellipseX - ellipseR && mouseX < ellipseX + ellipseR && mouseY > ellipseY - ellipseR && mouseY < ellipseY + ellipseR){
-        ellipseX = mouseX;
-        ellipseY = mouseY;
-    }
-    //change volume
-    var volume = map(ellipseX, startX, startX + rectW, 0, 1);
-    volume = constrain(volume, 0, 1);
-    sound.amp(volume);
-    //change frequency
-    var freq = map (ellipseY, startY, startY + rectH, 20,20000);
-    freq = constrain(freq,20,20000);
-    filter.freq(freq);
-    //change quad shape
-    rectWOffset = map(abs(ellipseX - centerX), 0, rectW/2, 0, 50);
-    rectHOffset = map(abs(ellipseY - centerY), 0, rectH/2, 0, 50);
-}
-
-function mousePressed(){
-    if (mouseX > 20 && mouseX < 105 && mouseY > 20 && mouseY < 75){
-        toggleSound();
-    }
-}
-
-function drawVisualization1() {
-    //get&map sound amplitude
-    ampLevel = amplitude.getLevel();
-    drawLine = map(ampLevel, 0, 0.1, 100, 800);
-    //astroid
-    beginShape();
-    stroke(163, 214, 245);
-    strokeWeight(0.5);
-    noFill();
-    translate(width / 2, height / 2);
-    for (var i = 0; i < drawLine / 2; i++) { //mouseX controls number of curves
-        LimMouseX = constrain(drawLine*2, 0, width);
-        var a = map(LimMouseX, 0, width, 10, 80); //relate to mouseX
-        var theta = map(i, 0, drawLine*2, 20, 360);
-        var x = 2 * a * cos(theta) + a * cos(2 * theta);
-        var y = 2 * a * sin(theta) - a * sin(2 * theta);
-        vertex(x, y);
-        endShape();
-        rotate(drawLine*2); //rotate according to position of mouseX
-    }
-}
-
-function drawVisualization2(controlFactor1, controlFactor2){
-
-    //Epicycloid Involute
-    beginShape();
-    stroke(242, 194, 203);
-    strokeWeight(0.5);
-    noFill();
-    for (var i = 0; i < controlFactor1; i ++){ //mouseX controls number of curves
-        LimMouseX = constrain(controlFactor1, 0, width);
-        var a = map(LimMouseX, 0, width, 0, 80); //relate to mouseX
-        var theta = map(i, 0, controlFactor1/5, 20, 360);
-        var b = map(controlFactor2, 0, height, 0, 50);
-        var x2 = (a+b)*cos(theta) - b*cos(((a+b)/b)*theta);
-        var y2 = (a+b)*sin(theta) - b*sin(((a+b)/b)*theta);
-        vertex(x2, y2);
-        endShape();
-    }
-}
-
-function drawMouseAction(){
-
-    var volume = map(mouseX, 0, width, 0, 1);
-    volume = constrain(volume, 0, 1);
-    sound.amp(volume);
-
-    // Set the rate to a range between 0.1 and 4
-    // Changing the rate alters the pitch
-    speed = map(mouseY, 0.1, height, 0, 2);
-    speed = constrain(speed, 0.01, 4);
-    sound.rate(speed);
-
-    // Draw some circles to show what is going on
-    stroke(0);
-    fill(255);
-    ellipse(mouseX, 100, 48, 48);
-    stroke(0);
-    fill(255);
-    ellipse(100, mouseY, 48, 48);
-
-    //visualize
-    visualizeMouseAction();
-
-}
-
-function visualizeMouseAction(){
-    //draw visualization
-    var level1 = map(speed, 0.01, 4, 100, 500);
-    var level2 = map(speed, 0.01, 4, 200, 400);
-    drawVisualization1();
-    drawVisualization2(level1, level2);
-}
-
+//use slider to manipulate sound
 function drawSliders(){
     //draw sliders
+    stroke(255);
     drawAmplitude();
     drawFrequency();
     drawReverb();
@@ -294,12 +249,13 @@ function drawSliders(){
     visualizeSliders();
 }
 
+//draw visualization corresponding to slider values
 function visualizeSliders(){
-    //draw visualization
     drawVisualization1();
     drawVisualization2(frequencyX, reverbX);
 }
 
+//draw amplitude slider
 function drawAmplitude(){
     //amplitude text
     textSize(22);
@@ -311,9 +267,10 @@ function drawAmplitude(){
     //amplitude slider
     strokeWeight(5);
     line(sliderStart, amplitudeY, sliderStop, amplitudeY);
-    ellipse(amplitudeX, amplitudeY, controlRadius*2);
+    ellipse(amplitudeX, amplitudeY, sliderBallRadius*2);
 }
 
+//draw frequency slider
 function drawFrequency(){
     //frequency text
     textSize(22);
@@ -325,9 +282,10 @@ function drawFrequency(){
     //frequency slider
     strokeWeight(5);
     line(sliderStart, frequencyY, sliderStop, frequencyY);
-    ellipse(frequencyX, frequencyY, controlRadius*2);
+    ellipse(frequencyX, frequencyY, sliderBallRadius*2);
 }
 
+//draw reverb slider
 function drawReverb(){
     //reverb text
     textSize(22);
@@ -339,20 +297,12 @@ function drawReverb(){
     //reverb slider
     strokeWeight(5);
     line(sliderStart, reverbY, sliderStop, reverbY);
-    ellipse(reverbX, reverbY, controlRadius*2);
+    ellipse(reverbX, reverbY, sliderBallRadius*2);
 }
 
-function drawSoundButton(){
-    //sound button
-    fill(255);
-    stroke(255);
-    rect(20, 20, 85, 55, 10);
-    fill(0);
-    triangle(86,47, 48, 28, 48, 68);
-}
-
+//frequency changes as frequency slider changes
 function changeFrequency(){
-    if (mouseY > (frequencyY - controlRadius) && mouseY < (frequencyY + controlRadius)) {
+    if (mouseY > (frequencyY - sliderBallRadius) && mouseY < (frequencyY + sliderBallRadius)) {
         frequencyX = constrain(mouseX, sliderStart, sliderStop);
         let level = map(frequencyX, sliderStart, sliderStop, 20,20000);
         filter.freq(level);
@@ -361,31 +311,40 @@ function changeFrequency(){
     }
 }
 
+//reverb changes as reverb slider changes
 function changeReverb(){
-    if (mouseY > (reverbY - controlRadius) && mouseY < (reverbY + controlRadius)) {
+    if (mouseY > (reverbY - sliderBallRadius) && mouseY < (reverbY + sliderBallRadius)) {
         reverbX = constrain(mouseX, sliderStart, sliderStop);
         let dryWet = constrain(map(reverbX, sliderStart, sliderStop, 0, 1), 0, 1);
         reverb.drywet(dryWet);
     }
 }
 
+//amplitude changes as amplitude slider changes
 function changeAmplitude(){
-    if (mouseY > (amplitudeY - controlRadius) && mouseY < (amplitudeY + controlRadius)) {
+    if (mouseY > (amplitudeY - sliderBallRadius) && mouseY < (amplitudeY + sliderBallRadius)) {
         amplitudeX = constrain(mouseX, sliderStart, sliderStop);
         let level = map(amplitudeX, sliderStart, sliderStop, 0,1);
         sound.amp(level);
     }
 }
 
+//play/pause sound
 function toggleSound() {
     if (sound.isPlaying() ){
+        //stop sound
         sound.stop();
+        //bounce circle - stop circle
         ellipseDeltaX = 0;
         ellipseDeltaY = 0;
     } else {
+        //play sound
         sound.play();
         sound.loop();
+        //bounce circle - move circle with a random direction & velocity
         ellipseDeltaX = random(-5, 5);
         ellipseDeltaY = random(-5, 5);
     }
 }
+
+
